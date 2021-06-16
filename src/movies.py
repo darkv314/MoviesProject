5cdfb57306e9db7238e752f65d007521510d3586
EC2 Default User <ec2-user@ip-172-31-41-164.us-east-2.compute.internal>
2021-06-16 03:18:36 +0000

    Error in swagger

--- src/movies.py
+++ src/movies.py
@@ -6,12 +6,12 @@ from boto3.dynamodb.conditions import Key, Attr
 users_table = os.environ['USERS_TABLE']
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(users_table)
+records ={}
 
 def getMovieInfo(event, context):
     path = event["path"]
     print(json.dumps(event))
     queryStringParameters = json.dumps(event["queryStringParameters"])
-    print(queryStringParameters)
     if queryStringParameters == "null":
         movie_id = path.split("/")[-1]
         response = table.get_item(
@@ -26,34 +26,56 @@ def getMovieInfo(event, context):
             'body': json.dumps(item)
             }
     else:
+        date = event["queryStringParameters"]["date"]
         response = table.scan(
-            FilterExpression=Attr(queryStringParameters["year"]).ge(2000)
+            FilterExpression=Attr('sk').begins_with(date) & Attr('pk').eq(movie_id)
         )
         items = response['Items']
-        print(items)
         return {
             'statusCode': 200,
-            'body': json.dumps("a")
+            'body': json.dumps(items)
             }
         
     
 def putMovieInfo(event, context):
-    print(json.dumps(event))
-    path = event["path"]
+    path = event['path']
     movie_id = path.split("/")[-1]
-    body = json.loads(event["body"])
-    
-    table.put_item(
-      Item={
-            'pk': movie_id,
-            'sk': 'info',
-            'title': body["title"],
-            'main_actor': body["main_actor"],
-            'year': body["year"]
+    body = json.loads(event['body'])
+    queryStringParameters = json.dumps(event["queryStringParameters"])
+    if queryStringParameters == "null":
+        table.put_item(
+          Item={
+                'pk': movie_id,
+                'sk': 'info',
+                'title': body["title"],
+                'main_actor': body["main_actor"],
+                'year': body["year"]
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Movie record saved!')
         }
-    )
-    # print(json.dumps({"state":"ok"}))
+    else:
+        date = event["queryStringParameters"]["date"]
+        if not date in records:
+            records[date] = 1
+        else: 
+            records[date] += 1
+        table.put_item(
+          Item={
+                'pk': movie_id,
+                'sk': date+"_sch"+str(records[date]),
+                'schedule': body["schedule"],
+                'cinema_room': body["cinema_room"]
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Movie date and schedule saved!')
+        }
+def getMoviePeople(event, context):
     return {
-        'statusCode': 200,
-        'body': json.dumps('Movie record saved!')
-    }
+            'statusCode': 200,
+            'body': json.dumps('Function Working')
+        }
